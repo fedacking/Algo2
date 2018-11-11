1023861341a92eb8f77466237a6142663e63248c
fedacking <31461406+fedacking@users.noreply.github.com>
2018-11-11 16:42:30 -0300

    First Commit

---
+++ src/main/Celda.java
@@ -0,0 +1,25 @@
+package main;
+
+public class Celda {
+	private int X;
+	private int Y;
+
+	public Celda(int x, int y) {
+		setX(x);
+		setY(y);
+	}
+
+	public int getX() {
+		return X;
+	}
+	public void setX(int x) {
+		X = x;
+	}
+	public int getY() {
+		return Y;
+	}
+	public void setY(int y) {
+		Y = y;
+	}
+
+}
---
+++ test/main/CeldaBasicTest.java
@@ -0,0 +1,18 @@
+package main;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import main.Celda;
+
+
+public class CeldaBasicTest {
+	
+    @Test
+    public void inicializarCeldaEn11yTestear() {
+    	Celda celdaTest = new Celda(1,1);
+
+    	assertEquals(1,celdaTest.getX(), "Getter y Setter test de X");
+    	assertEquals(1,celdaTest.getY(), "Getter y Setter test de Y");
+    }
+}
